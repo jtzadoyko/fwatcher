2a4be2cd7a3cc89cf3a60d8cee5200dee17569cd
Josh <44326766+jtzadoyko@users.noreply.github.com>
2020-03-04 11:47:39 -0500

    Create FILE_WATCHER.ps1

---
+++ FILE_WATCHER.ps1
@@ -0,0 +1,18 @@
+﻿$FileSystemWatcher = New-Object System.IO.FileSystemWatcher 
+$FileSystemWatcher.Path = 'C://Users/SDG/Desktop/file_watch'
+
+
+Register-ObjectEvent -InputObject $FileSystemWatcher  -EventName Created  -Action {
+    $Object  = "{0} was  {1} at {2}" -f $Event.SourceEventArgs.FullPath,
+    $Event.SourceEventArgs.ChangeType,
+    $Event.TimeGenerated
+    
+    $WriteHostParams  = @{
+    ForegroundColor = 'Green'
+    BackgroundColor =  'Black'
+    Object =  $Object
+    }
+    
+    Write-Host @WriteHostParams
+
+} 
